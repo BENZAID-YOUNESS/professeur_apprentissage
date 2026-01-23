bda2f67940065b2cfe87ad7942e8f2587f95eb4b
Youness Benzaid <benzaid.youness@outlook.com>
2026-01-23 09:41:11 +0100

    ajout exercices + mission 5e et mise à jour mission 4d

---
+++ ex3.py
@@ -0,0 +1,28 @@
+shopping = []
+
+def affiche_menu():
+
+    print("\n====== MENU SHOPPING ======")
+    print("1 - Ajouter un article")
+    print("2 - Supprimer un article (par nom)")
+    print("3 - Afficher la liste")
+    print("4 - Trier la liste")
+    print("5 - Supprimer par numéro (pop)")
+    print("6 - Afficher le nombre d’articles")
+    print("7 - Quitter")
+
+def main():
+    while True:
+        affiche_menu()   
+        choix = input("quel est ton choix ? ") 
+        choix == "1"
+        article = input("ajouter un article : ")
+        if article not in shopping:
+            shopping.append(article)
+            print(f"{article} : à était ajouter .", shopping)
+            #continue
+        else:
+            print(f"{article} : éxiste déja dans la liste .", shopping)
+
+if __name__ == "__main":
+    main()
--- mission_4d.py
+++ mission_4d.py
@@ -1,10 +1,10 @@
 shopping = []
 
 def afficher_menu():
-    print("1 - Ajouter")
-    print("2 - Supprimer")
-    print("3 - Afficher")
-    print("4 - Trier")
+    print("1 - Ajouter un article : ")
+    print("2 - Supprimer un article : ")
+    print("3 - Afficher les articles : ")
+    print("4 - Trier les article par ordre alphabetique : ")
     print("5 - Quitter")
 
 def main():
@@ -14,7 +14,7 @@ def main():
         choix = input("quel est ton choix ?: ").strip().lower()
 
         if choix == "1":
-            article = input("ajouter un article : ")
+            article = input("ajouter un article : ").strip().lower()
             
             if article not in shopping:
                 shopping.append(article)
@@ -22,7 +22,13 @@ def main():
             else:
                 print(f"{article} : deja dans la liste .")
 
-                print("articles dans la liste :", shopping)
+            print("articles dans la liste :", shopping)
+            continue
+
+        if choix == "5":
+            input("vous ete sur le point de quitter : ")
+            print("exit..!")
+            break
 
 if __name__ == "__main__":
     main()
